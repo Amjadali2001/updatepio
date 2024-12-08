6f31a7ff443249c54c675c6d41a48a75462e9011
amjad <amjadsaid9925@gmail.com>
2024-12-08 11:14:30 +0500

    code update

--- components/sidebar/menu-icon.tsx
+++ components/sidebar/menu-icon.tsx
@@ -1,11 +1,23 @@
+// "use client";
+
+// import { LucideIcon } from "lucide-react";
+
+// interface MenuIconProps {
+//   Icon: LucideIcon;
+// }
+
+// export function MenuIcon({ Icon }: MenuIconProps) {
+//   return <Icon className="h-4 w-4" />;
+// }
+
 "use client";
 
-import { LucideIcon } from "lucide-react";
+import { LucideIcon, LucideProps } from "lucide-react";
 
 interface MenuIconProps {
-  Icon: LucideIcon;
+  Icon: React.ComponentType<LucideProps>; // Correct type for Lucide Icons
 }
 
 export function MenuIcon({ Icon }: MenuIconProps) {
-  return <Icon className="h-4 w-4" />;
+  return <Icon className="h-4 w-4" />; // Render the icon with styling
 }
